8060b207c05d4c88e9df0bc3499f7e13152c3ff8
Alexsandr Salamatov <ch1sel@ya.ru>
2018-11-04 00:43:41 +0300

    add HttpResponseMessage Extensions

---
+++ src/JStreamAsyncNet/HttpResponseMessageExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JStreamAsyncNet
+{
+	public static class HttpResponseMessageExtensions
+	{
+		public static async Task<T> ToObject<T>(this Task<HttpResponseMessage> response)
+		{
+			HttpResponseMessage result = await response;
+
+			result.EnsureSuccessStatusCode();
+
+			return await result.Content.ReadAsStreamAsync().ToObject<T>().ConfigureAwait(false);
+		}
+
+		public static async Task<T[]> ToArray<T>(this Task<HttpResponseMessage> response)
+		{
+			HttpResponseMessage result = await response;
+
+			result.EnsureSuccessStatusCode();
+
+			return await result.Content.ReadAsStreamAsync().ToArray<T>().ConfigureAwait(false);
+		}
+	}
+}
