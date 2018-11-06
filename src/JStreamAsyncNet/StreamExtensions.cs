c1cfb3f31a1a67b43c5f8a35329a23b28737d7dc
Alexander Salamatov <asalamatov@appulate.com>
2018-11-06 16:22:16 +0300

    add SteamExtensions version 0.0.8

--- src/JStreamAsyncNet/StreamExtensions.cs
+++ src/JStreamAsyncNet/StreamExtensions.cs
@@ -6,14 +6,14 @@ using Newtonsoft.Json.Linq;
 
 namespace JStreamAsyncNet
 {
-	public static class StreamAsyncExtensions
+	public static class StreamExtensions
 	{
-		public static async Task<T> ToObject<T>(this Task<Stream> stream, JsonSerializer serializer = null,
+		public static async Task<T> ToObject<T>(this Stream stream, JsonSerializer serializer = null,
 			CancellationToken cancellationToken = default)
 		{
 			using (stream)
 			{
-				using (var streamReader = new StreamReader(await stream.ConfigureAwait(false)))
+				using (var streamReader = new StreamReader(stream))
 				{
 					using (var reader = new JsonTextReader(streamReader))
 					{
@@ -24,12 +24,12 @@ namespace JStreamAsyncNet
 			}
 		}
 
-		public static async Task<T[]> ToArray<T>(this Task<Stream> stream, JsonSerializer serializer = null,
+		public static async Task<T[]> ToArray<T>(this Stream stream, JsonSerializer serializer = null,
 			CancellationToken cancellationToken = default)
 		{
 			using (stream)
 			{
-				using (var streamReader = new StreamReader(await stream.ConfigureAwait(false)))
+				using (var streamReader = new StreamReader(stream))
 				{
 					using (var reader = new JsonTextReader(streamReader))
 					{
@@ -40,12 +40,12 @@ namespace JStreamAsyncNet
 			}
 		}
 
-		public static async Task FromObject<T>(this Task<Stream> stream, T @object, JsonSerializer serializer = null,
+		public static async Task FromObject<T>(this Stream stream, T @object, JsonSerializer serializer = null,
 			CancellationToken cancellationToken = default)
 		{
 			using (stream)
 			{
-				using (var writer = new StreamWriter(await stream.ConfigureAwait(false)))
+				using (var writer = new StreamWriter(stream))
 				{
 					JsonWriter jsonTextWriter = new JsonTextWriter(writer);
 					await JObject.FromObject(@object, serializer ?? new JsonSerializer())
@@ -54,18 +54,50 @@ namespace JStreamAsyncNet
 			}
 		}
 
-		public static async Task FromArray<T>(this Task<Stream> stream, T[] array, JsonSerializer serializer = null,
+		public static async Task FromArray<T>(this Stream stream, T[] array, JsonSerializer serializer = null,
 			CancellationToken cancellationToken = default)
 		{
 			using (stream)
 			{
-				using (var writer = new StreamWriter(await stream.ConfigureAwait(false)))
+				using (var writer = new StreamWriter(stream))
 				{
 					JsonWriter jsonTextWriter = new JsonTextWriter(writer);
 					await JArray.FromObject(array, serializer ?? new JsonSerializer())
 						.WriteToAsync(jsonTextWriter, cancellationToken).ConfigureAwait(false);
 				}
 			}
+		}
+	}
+
+	public static class StreamAsyncExtensions
+	{
+		public static async Task<T> ToObject<T>(this Task<Stream> streamTask, JsonSerializer serializer = null,
+			CancellationToken cancellationToken = default)
+		{
+			var stream = await streamTask.ConfigureAwait(false);
+			return await stream.ToObject<T>(serializer, cancellationToken).ConfigureAwait(false);
+		}
+
+		public static async Task<T[]> ToArray<T>(this Task<Stream> streamTask, JsonSerializer serializer = null,
+			CancellationToken cancellationToken = default)
+		{
+			var stream = await streamTask.ConfigureAwait(false);
+			return await stream.ToArray<T>(serializer, cancellationToken).ConfigureAwait(false);
+		}
+
+		public static async Task FromObject<T>(this Task<Stream> streamTask, T @object,
+			JsonSerializer serializer = null,
+			CancellationToken cancellationToken = default)
+		{
+			var stream = await streamTask.ConfigureAwait(false);
+			await stream.FromObject(@object, serializer, cancellationToken).ConfigureAwait(false);
+		}
+
+		public static async Task FromArray<T>(this Task<Stream> streamTask, T[] array, JsonSerializer serializer = null,
+			CancellationToken cancellationToken = default)
+		{
+			var stream = await streamTask.ConfigureAwait(false);
+			await stream.FromArray(array, serializer, cancellationToken).ConfigureAwait(false);
 		}
 	}
 }
